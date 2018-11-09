4a7faa3c73330988ff7fc8c062db0ae96aeb6272
igorjaniszewski78552 <igor.janiszewski@lo1.sandomierz.pl>
2018-11-09 10:36:11 +0100

    instrukcja warunkowa

--- python/bmi.py
+++ python/bmi.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+#
+#  bmi.py
+#  bmi = masa / (wzrost* wzrost)
+#  BMI < 18,5 - niedowaga
+#  BMI  18,5-24,9 - norma
+#  BMI > = 25 - nadwaga
+#  BMI >= 30 - otyłość 
 
 
 def main(args):
-    
-    a = input('Podaj bok a:')
-    b = input('Podaj bok b:')
-
-
-    obwod = 2 * (int(a) + int(b))
-    print(obwod)
-
     return 0
 
 if __name__ == '__main__':
     import sys
     sys.exit(main(sys.argv))
-      
--- python/szkielet.py
+++ python/szkielet.py
@@ -2,19 +2,17 @@
 # -*- coding: utf-8 -*-
 
 
-
 def main(args):
-    
     a = input('Podaj bok a:')
     b = input('Podaj bok b:')
+    c = input('Podaj bok c:')
 
-
-    obwod = 2 * (int(a) + int(b))
-    print(obwod)
+    if a + b > c:
+        if a + c >b:
 
     return 0
+
 
 if __name__ == '__main__':
     import sys
     sys.exit(main(sys.argv))
-      
