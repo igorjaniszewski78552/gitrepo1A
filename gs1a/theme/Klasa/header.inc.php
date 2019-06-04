3490e55d57d1c455ee0afdcc37e957fc06a71381
igorjaniszewski78552 <igor.janiszewski@lo1.sandomierz.pl>
2019-06-04 16:31:40 +0200

    j

--- gs1a/theme/Klasa/header.inc.php
+++ gs1a/theme/Klasa/header.inc.php
@@ -10,7 +10,7 @@
    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <!-- jQuery library -->
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-   <link rel="stylesheet" href="css/klasa.css">
+   <link rel="stylesheet" href="<?php get_theme_url(); ?>/style.css">
    <?php get_header(); ?>
 </head>
 
