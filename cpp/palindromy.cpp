21e99e843d867207b231acac60809a3b7f60cc80
igorjaniszewski78552 <igor.janiszewski@lo1.sandomierz.pl>
2019-11-18 10:35:36 +0100

    oikadhij

--- cpp/palindromy.cpp
+++ cpp/palindromy.cpp
@@ -21,43 +21,60 @@
  * 
  */
 
-using namespace std;
 #include <iostream>
 #include <cstring>
-bool czy_palindrom(char tab[]){
-	int rozmiar = strlen(tab);
-	int n = 0;
-cout << rozmiar;
-	for (int i = 0; i < rozmiar / 2; i++){
-		if (i == rozmiar - i -1){
-			n += 1;
-	}
-	else if(i == rozmiar){
-		
-		
-	}
-	else{
-		n = 0;
-	} 
-	
-	}
-	if (n == rozmiar){
-		cout <<"to palindrom";
-}
-else{
-	cout<< "to nie palidnrom";
-}
-return n;
-}
 
+using namespace std;
+
+
+
+bool czy_palindrom(char tab[]){
+	int rozmiar = strlen(tab) - 1;
+	for(int i = 0; i < rozmiar/2; i++){
+		if(tab[i] == tab[rozmiar-1 - i])
+			;
+		else
+			return false;
+			}
+		
+		return true;
+	}
+
+void usunSpace(char tab1[], char tab2[]){
+	int rozmiar = strlen(tab1);
+	for(int i = 0; i < rozmiar; i++){
+		if(tab1[i] != ' '){
+			tab2[i] = tab1[i];
+			}
+		
+	}
+}
+bool czy_palindrom2(char tab[]){
+	int rozmiar = strlen(tab);
+	for(int i = 0; i < rozmiar/2; i++){
+		if(tab[i] == ' ') i++;
+		if(tab[i] == tab[rozmiar- 1 - i])
+			;
+		else
+			return false;
+			}
+		
+		return true;
+	}
 int main(int argc, char **argv)
 {
 	int r = 20;
-	char napis[r];
-	cin.getline(napis, 20);
-	cout << napis;
-	czy_palindrom(napis);
-	
+	char napis1[r];
+	char napis2[r];
+	cin.getline(napis1, 20);
+	usunSpace(napis1, napis2);
+	cout << napis2 << endl;
+	cout << strlen(napis2) << endl;
+	if(czy_palindrom(napis2))
+		cout << "To palindrom!" << endl;
+		else{
+			cout << "To nie palindrom" << endl;
+	}
 	return 0;
 }
 
