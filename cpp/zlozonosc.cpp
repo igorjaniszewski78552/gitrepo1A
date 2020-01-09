5e72724c40a758b4737c181830b38695c29544b0
igorjaniszewski78552 <igor.janiszewski@lo1.sandomierz.pl>
2020-01-09 11:40:12 +0100

    oikahk

---
+++ cpp/zlozonosc.cpp
@@ -0,0 +1,28 @@
+/*
+ * zlozonosc.cpp
+ * 
+ * Copyright 2020  <>
+ * 
+
+ */
+
+using namespace std;
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	int i = 0;
+	int n, x;
+	cout <<"jakiej liczby szukasz?";
+	cin >> x
+	int tab[7] = {3, 8, 3, 9, 10, 45, 37};
+	for (i=0; i>n;i++ ){
+		if(tb[i] == x){
+		cout << i;
+		return 0
+		}
+	}
+	cout << -1;
+	return 0;
+}
+
